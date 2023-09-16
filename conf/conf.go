f5406ac92479ab983e60faf959c73ae91e33538e
Cameron Steele <cam.steeleis@gmail.com>
2023-09-16 14:11:00 -0500

    Add moodle Secrets

--- conf/conf.go
+++ conf/conf.go
@@ -11,4 +11,8 @@ type Env struct {
 	SelfURL          string `split_words:"true" required:"true"`
 	StripeKey        string `split_words:"true"`
 	StripeWebhookKey string `split_words:"true"`
+
+	MoodleURL     string `split_words:"true" required:"true"`
+	MoodleWSToken string `split_words:"true" required:"true"`
+	MoodleSecret  string `split_words:"true" required:"true"`
 }
