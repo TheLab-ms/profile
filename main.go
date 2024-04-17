3fce03e1d4cd958df08194bf38041965069ab884
Jordan Olshevski <jordanolshevski@gmail.com>
2024-04-17 17:22:45 -0500

    Remove file server support

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/stripe/stripe-go/v75"
 
@@ -28,24 +27,7 @@ var rawTemplates embed.FS
 
 var templates *template.Template
 
-var (
-	inFlightGauge = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "in_flight_requests",
-		Help: "A gauge of requests currently being served",
-	})
-
-	requestCounter = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "requests_total",
-			Help: "HTTP request counter by code and method",
-		},
-		[]string{"code", "method"},
-	)
-)
-
 func init() {
-	prometheus.MustRegister(inFlightGauge, requestCounter)
-
 	// Parse the embedded templates once during initialization
 	var err error
 	templates, err = template.ParseFS(rawTemplates, "templates/*")
@@ -102,8 +84,5 @@ func main() {
 		Assets:      assets,
 		Templates:   templates,
 	}
-	log.Fatal(http.ListenAndServe(":8080",
-		promhttp.InstrumentHandlerInFlight(inFlightGauge,
-			promhttp.InstrumentHandlerCounter(requestCounter,
-				svr.NewHandler()))))
+	log.Fatal(http.ListenAndServe(":8080", svr.NewHandler()))
 }
