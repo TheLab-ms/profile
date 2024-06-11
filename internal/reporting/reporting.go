1dfa06c85a3266270dd9788a25a887c923b9607b
Jordan Olshevski <jordanolshevski@gmail.com>
2024-06-10 19:35:32 -0500

    Fix error handling

--- internal/reporting/reporting.go
+++ internal/reporting/reporting.go
@@ -2,8 +2,6 @@ package reporting
 
 import (
 	"context"
-	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 	"strings"
@@ -104,9 +102,9 @@ func (s *ReportingSink) WriteMetrics(counters *Counters) error {
 
 func (s *ReportingSink) GetLatestSwipe(ctx context.Context, name string, last time.Time) (time.Time, bool, error) {
 	swipe := time.Time{}
-	err := s.db.QueryRow(ctx, "SELECT time FROM swipes WHERE name == $1 AND time > $2 ORDER BY time DESC LIMIT 1", name, last).Scan(&swipe)
-	if errors.Is(err, sql.ErrNoRows) {
-		return swipe, false, nil
+	err := s.db.QueryRow(ctx, "SELECT time FROM swipes WHERE name = $1 AND time > $2 ORDER BY time DESC LIMIT 1", name, last).Scan(&swipe)
+	if strings.Contains(err.Error(), "no rows in result set") {
+		return swipe, false, nil // errors.Is didn't work with the psql library for some reason
 	}
 	return swipe, true, err
 }
